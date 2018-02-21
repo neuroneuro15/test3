534e364b0448c42938a133d016fb3adc2a292e4e
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-02-21 16:03:08 +0100

    removed checkpoints

--- .ipynb_checkpoints/analyze_meetup_attendance-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-
-
-url = 'https://api.meetup.com/SuperPythonTalks/events?&status=past'
-r = requests.get(url)
-
-df = pd.DataFrame(r.json())
-df = df[['name', 'local_date', 'yes_rsvp_count']]
