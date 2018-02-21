e637ac1eba776ea2538b59fd964e7463bafe5f16
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-02-21 16:10:23 +0100

    added second file

--- .ipynb_checkpoints/analyze_meetup_attendance-checkpoint.py
+++ .ipynb_checkpoints/analyze_meetup_attendance-checkpoint.py
@@ -1,9 +1,12 @@
 import requests
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 url = 'https://api.meetup.com/SuperPythonTalks/events?&status=past'
 r = requests.get(url)
 
 df = pd.DataFrame(r.json())
 df = df[['name', 'local_date', 'yes_rsvp_count']]
+
+# Plot
+df.plot(x='local_date', y='yes_rsvp_count')
--- plot_attendance.py
+++ plot_attendance.py
@@ -2,8 +2,9 @@ import requests
 import pandas as pd
 
 
-url = 'https://api.meetup.com/SuperPythonTalks/events?&status=past'
+url = 'https://api.meetup.com/PyData-Munchen/events?&status=past'
 r = requests.get(url)
 
 df = pd.DataFrame(r.json())
 df = df[['name', 'local_date', 'yes_rsvp_count']]
+df.plot(x='local_date', y='yes_rsvp_count')
